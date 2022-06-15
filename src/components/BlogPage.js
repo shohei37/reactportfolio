0ddd38a704b20fbf76ba30c898459788fbbfb98f
shohei37 <syttpp@gmail.com>
2022-06-15 13:00:29 +0900

    first commit

---
+++ src/components/BlogPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='container'>
+      <h1>ブログ</h1>
+      <p>lorem</p>
+      <p>lorem100</p>
+      <p>lorem100</p>
+      <p>lorem100</p>
+    </div>
+  )
+}
+
+export default BlogPage
